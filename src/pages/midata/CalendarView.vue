3b8c2890113671ad9a39201b1b11a3fa3f778e42
Dominic Willi <dominic.willi@students.bfh.ch>
2023-11-09 15:46:10 +0100

    update

--- src/pages/midata/CalendarView.vue
+++ src/pages/midata/CalendarView.vue
@@ -33,21 +33,96 @@
         </q-date> </q-popup-proxy
     ></q-btn>
   </q-card>
-  <q-card>
-    <q-tab-panels v-model="proxyDate" animated>
-      <q-tab-panel name="10.02.2023">
-        <div class="text-h4 q-mb-md">Termin 1</div>
-        Einzeltherapie
-      </q-tab-panel>
-      <q-tab-panel name="05.02.2023">
-        <div class="text-h4 q-mb-md">Termin 2</div>
-        Einzeltherapie
-      </q-tab-panel>
-      <q-tab-panel name="06.02.2023">
-        <div class="text-h4 q-mb-md">Termin 3</div>
-        Gruppentherapie
-      </q-tab-panel>
-    </q-tab-panels>
+
+  <q-card v-if="proxyDate === '10.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 1</div>
+      Einzeltherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '05.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 2</div>
+      Einzeltherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '06.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 3</div>
+      Gruppentherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '06.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 3</div>
+      Gruppentherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '07.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 4</div>
+      Einzeltherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '08.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 5</div>
+      Paartherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '09.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 6</div>
+      Musiktherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '10.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 7</div>
+      Kunsttherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '11.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 8</div>
+      Beratungsgespräch
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '12.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 9</div>
+      Verhaltenstherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '13.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 10</div>
+      Ernährungsberatung
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '14.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 11</div>
+      Sporttherapie
+    </q-card-section>
+  </q-card>
+
+  <q-card v-if="proxyDate === '15.11.2023'">
+    <q-card-section>
+      <div class="text-h4 q-mb-md">Termin 12</div>
+      Achtsamkeitstraining
+    </q-card-section>
   </q-card>
 </template>
 
@@ -56,6 +131,5 @@ import { ref } from 'vue';
 const date = new Date().toLocaleDateString('de-De');
 const proxyDate = ref(date);
 
-// const date = new Date();
-const events = ['2023/02/01', '2023/02/05', '2023/02/06', '2023/02/10'];
+const events = ['2023/11/10', '2023/11/17', '2023/11/13', '2023/11/12'];
 </script>
