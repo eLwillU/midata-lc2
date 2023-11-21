ae1ef729df1855ac1c729704be2848a6f82b9e8f
karinke1234 <kennk1@bfh.ch>
2023-11-21 11:57:40 +0100

    removed calendar

--- src/pages/midata/WeekView.vue
+++ src/pages/midata/WeekView.vue
@@ -1,16 +1,6 @@
 <template>
   <!-- Verwendung der GreetingCard Komponente mit dynamischem Text -->
   <greetings-card :name="user.name" :message="todaysMessage" />
-  <div style="max-width: 800px; width: 100%">
-    <q-calendar
-      v-model="selectedDate"
-      view="week"
-      hour24-format
-      :start-hour="8"
-      :end-hour="22"
-      locale="en-us"
-    />
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,5 +12,4 @@ const user = ref({
 });
 
 const todaysMessage = ref('diese Woche steht folgendes an');
-const selectedDate = ref(new Date());
 </script>
