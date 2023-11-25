67b19f40ffe1ef7160abd2b87e2ec25cbc32eed7
Dominic <dominic.willi@students.bfh.ch>
2023-11-25 01:10:04 +0100

    added file upload

--- src/pages/AddAppointment.vue
+++ src/pages/AddAppointment.vue
@@ -1,30 +1,48 @@
 <template>
   <div>Termin-Template hinzufügen</div>
   <div>
-    <q-file
-      filled
-      bottom-slots
-      v-model="model"
-      label="FHIR .json Datei auswählen"
-      counter
-    >
+    <q-file v-model="uploadedFile" label="FHIR .json Datei auswählen" counter>
       <template v-slot:prepend>
         <q-icon name="cloud_upload" @click.stop.prevent />
       </template>
       <template v-slot:append>
         <q-icon
           name="close"
-          @click.stop.prevent="model = null"
+          @click.stop.prevent="uploadedFile = null"
           class="cursor-pointer"
+          :disabled="!uploadedFile"
         />
       </template>
     </q-file>
   </div>
-  <q-btn @click="$midata.getPatientResource()">Patient-Ressource Getten</q-btn>
+  <q-btn @click="parseFile">Patient-Ressource Getten</q-btn>
   <q-btn @click="$midata.createObservation()">Observation Createn</q-btn>
 </template>
 
-<script setup>
+<script>
+import { ref } from 'vue';
 
+export default {
+  setup() {
+    const uploadedFile = ref(null);
+    let tempFile = new File([''], '');
+    const fileReader = new FileReader();
 
+    // Add a load event listener to handle the file read completion
+    fileReader.onload = function (event) {
+      console.log(event.target.result);
+    };
+
+    function parseFile() {
+      if (uploadedFile.value) {
+        tempFile = uploadedFile.value;
+        fileReader.readAsText(tempFile);
+      } else {
+        console.log('No file selected');
+      }
+    }
+
+    return { parseFile, uploadedFile, fileReader };
+  },
+};
 </script>
