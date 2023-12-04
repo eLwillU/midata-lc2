f6b896ff85ea0a5f7e01f9ac5f0c2d898cb7330d
karinke1234 <kennk1@bfh.ch>
2023-12-04 22:14:59 +0100

    update

--- src/components/AppointmentCard.vue
+++ src/components/AppointmentCard.vue
@@ -1,7 +1,96 @@
 <template>
-  <q-card>
-    <div>Termin Blabla</div>
-  </q-card>
+  <q-table
+  flat
+  bordered
+  grid
+  title="Wochenplan"
+  :rows="rows"
+  :columns="columns"
+  row-key="time"
+  :separator="separator"
+  :rows-per-page-options="[1, 3, 5, 7, 10, 15, 20, 25, 50, 0]"
+>
+  <template v-slot:body-cell-monday="props">
+    <q-td :props="props" :style="getCellStyle(props)">
+      {{ props.row.monday }}
+    </q-td>
+  </template>
+</q-table>
 </template>
 
-<script setup></script>
+<script setup>
+
+
+import {ref } from 'vue'
+
+const columns = ref([
+  {
+    name: 'time',
+    label: 'Zeit',
+    field: (row) => row.time,
+  },
+  {
+    name: 'monday',
+    label: 'Montag',
+    field: (row) => row.monday,
+  },
+  {
+    name: 'tuesday',
+    label: 'Dienstag',
+    field: (row) => row.tuesday,
+  },
+  {
+    name: 'wednesday',
+    label: 'Mittwoch',
+    field: (row) => row.wednesday,
+  },
+  {
+    name: 'thursday',
+    label: 'Donnerstag',
+    field: (row) => row.thursday,
+  },
+  {
+    name: 'friday',
+    label: 'Freitag',
+    field: (row) => row.friday,
+  },
+]);
+
+const rows = ref([
+  {
+    name: 'Tag 1',
+    time: '08:00 - 09:00',
+    monday: 'Einzeltherapie',
+    tuesday: 'Gruppentherapie',
+    wednesday: 'Biologie',
+    thursday: 'Chemie',
+    friday: 'Sport',
+    color: 'blue',
+  },
+  {
+    name: 'Tag 2',
+    time: '09:00 - 10:00',
+    monday: 'Deutsch',
+    tuesday: 'Physik',
+    wednesday: 'Kunst',
+    thursday: 'Musik',
+    friday: 'Geschichte',
+    color: 'red',
+  },
+
+  {
+    name: 'Tag 2',
+    time: '09:00 - 10:00',
+    monday: 'Deutsch',
+    tuesday: 'Physik',
+    wednesday: '',
+    thursday: 'Musik',
+    friday: 'Geschichte',
+    color: 'green',
+  },
+]);
+
+
+
+
+</script>
--- src/pages/midata/DayView.vue
+++ src/pages/midata/DayView.vue
@@ -4,38 +4,21 @@
     <!-- Verwendung der GreetingCard Komponente mit dynamischem Text -->
     <greetings-card :name="user.name" :message="todaysMessage" />
     <date-display-card :current-date="currentDate" class="q-mb-lg" />
-
-    <q-card
-      class="my-card q-mt-md"
-      style="background-color: #1976d2; color: white"
-    >
-      <q-card-section class="q-pa-md">
-        <div class="text-h6">Einzeltherapie</div>
-        <div class="text-caption">14:30–15:00</div>
-      </q-card-section>
-    </q-card>
-
-    <q-card
-      class="my-card q-mt-md"
-      style="background-color: #5e35b1; color: white"
-    >
-      <q-card-section class="q-pa-md">
-        <div class="text-h6">Abendessen bestellen</div>
-        <div class="text-caption">17:00</div>
-      </q-card-section>
-    </q-card>
-  </div>
+    <appointment-card></appointment-card>
+</div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import DateDisplayCard from 'src/components/DateDisplayCard.vue';
 import GreetingsCard from 'src/components/GreetingsCard.vue';
+import AppointmentCard from 'src/components/AppointmentCard'
 
 export default {
   components: {
     GreetingsCard, // Komponente registrieren
     DateDisplayCard,
+    AppointmentCard
   },
   setup() {
     const currentDate = ref(new Date());
