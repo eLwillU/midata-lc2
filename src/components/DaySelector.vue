2f3c0c3ba9d52f6a681ff7fce9738899a9d06b43
Dominic Willi <dominic.willi@students.bfh.ch>
2023-12-14 13:13:56 +0100

    updated daySelector

--- src/components/DaySelector.vue
+++ src/components/DaySelector.vue
@@ -1,23 +1,14 @@
 <template>
   <div class="row justify-evenly items-center q-mb-sm">
-    <q-btn @click="addDay" class="q-px-sm">
+    <q-btn @click="removeDay" class="q-px-sm">
       <q-icon name="arrow_back" size="xs" />
     </q-btn>
-    <q-btn>Heute</q-btn>
-    <q-input
-      v-model="text"
-      :dense="dense"
-      :placeholder="currentDay"
-      class="col-5"
-      standout
-      disable
-    >
-      <template v-slot:prepend>
-        <q-icon name="event" />
-      </template>
-    </q-input>
+    <q-btn @click="setToday">Heute</q-btn>
 
-    <q-btn @click="removeDay" class="q-px-sm">
+    <q-btn color="grey-7" outline class="row justify-evenly"
+      ><q-icon name="event" />{{ currentDay }}</q-btn
+    >
+    <q-btn @click="addDay" class="q-px-sm">
       <q-icon name="arrow_forward" size="xs"
     /></q-btn>
   </div>
@@ -29,38 +20,33 @@
 
 <script setup>
 import { ref } from 'vue';
+import { date } from 'quasar';
 
-const currentDay = ref(new Date().toLocaleDateString());
-const previousDay = ref(new Date().toLocaleDateString());
-const nextDay = ref(new Date().toLocaleDateString());
+const currentDay = ref(new Date());
 const countDays = ref(0);
 
 setDays();
 
 function setDays() {
   const baseDate = new Date();
-
-  // Update the dates with formatted strings
-  currentDay.value = new Date(
-    baseDate.setDate(baseDate.getDate() + countDays.value),
-  ).toLocaleDateString();
-  nextDay.value = new Date(
-    baseDate.setDate(baseDate.getDate() + 1),
-  ).toLocaleDateString();
-  previousDay.value = new Date(
-    baseDate.setDate(baseDate.getDate() - 2),
-  ).toLocaleDateString();
+  currentDay.value = date.formatDate(
+    date.addToDate(baseDate, { days: countDays.value }),
+    'DD.MM.YYYY',
+  );
 }
 
 function addDay() {
   countDays.value += 1;
-  console.log(countDays);
   setDays();
 }
 
 function removeDay() {
   countDays.value -= 1;
-  console.log(countDays);
+  setDays();
+}
+
+function setToday() {
+  countDays.value = 0;
   setDays();
 }
 </script>
