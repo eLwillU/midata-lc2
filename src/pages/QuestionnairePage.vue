c623c92407bae22db2e0af989c2e91ad513ffee0
karinke1234 <kennk1@bfh.ch>
2023-12-30 12:03:02 +0100

    Fragebogen Update

--- src/pages/QuestionnairePage.vue
+++ src/pages/QuestionnairePage.vue
@@ -7,33 +7,52 @@
       />
       <q-card-section>
         <div class="text-h6">
-          Zürcher Fragebogen zur Patientenzufriedenheit in der psychiatrischen
-          Klinik
+          Zürcher Patientenzufriedenheits-Fragebogen für Psychiatrie
         </div>
         <div class="text-subtitle2">(Züpaz)</div>
       </q-card-section>
       <q-card-section class="q-pt-none">{{ content }}</q-card-section>
     </q-card>
   </div>
-
+  <!-- Fortschrittsbalken -->
+  <div class="q-pa-md" v-if="loaded && display">
+    <q-linear-progress size="25px" :value="progressValue" color="primary">
+      <div class="absolute-full flex flex-center">
+        <q-badge color="white" text-color="primary" :label="progressLabel" />
+      </div>
+    </q-linear-progress>
+  </div>
   <!-- Fragebogen-Bereich wird nur angezeigt, wenn display true ist -->
   <div v-if="loaded && display">
-    <div
-      v-for="item in selectedQuestionnaire.item"
-      :key="item.linkId"
-      class="q-mb-sm"
-    >
+    <div v-for="item in displayedQuestions" :key="item.linkId" class="q-mb-sm">
       <q-card>
         <q-card-section>
           <QuestionItem :question="item" v-model="item.selected"></QuestionItem>
         </q-card-section>
       </q-card>
     </div>
-    <q-btn color="primary" @click="evaluateSelectedValues"
-      >Evaluate Selected Values</q-btn
-    >
+    <!-- Navigationsbuttons für Weiter und Zurück -->
+    <div class="row justify-between q-mt-md">
+      <q-btn
+        v-if="currentPage.value > 1"
+        @click="previousPage"
+        label="Zurück"
+        color="primary"
+      />
+      <q-btn
+        v-if="hasMorePages"
+        @click="nextPage"
+        label="Weiter"
+        color="primary"
+      />
+      <q-btn
+        v-if="isLastPage"
+        @click="evaluateSelectedValues"
+        label="Auswerten"
+        color="primary"
+      />
+    </div>
   </div>
-
   <!-- Neue q-card mit Dankesnachricht -->
   <div v-if="showThankYouCard" class="q-mb-md">
     <q-card class="my-card">
@@ -51,7 +70,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref, toRaw } from 'vue';
+import { onMounted, ref, toRaw, computed } from 'vue';
 import { midata } from 'src/boot/plugins';
 import QuestionItem from 'src/components/QuestionItem.vue';
 
@@ -59,14 +78,14 @@ const selectedQuestionnaire = ref(null);
 const loaded = ref(false);
 const display = ref(true);
 const showThankYouCard = ref(false);
+const currentPage = ref(1);
+const questionsPerPage = 5;
 
 const content =
-  'Wir sind sehr daran interessiert, Ihre Meinung zu Ihrem kürzlich erfolgten Aufenthalt in unserer Klinik zu erfahren. ' +
-  'Auf dieser und den folgenden Seiten finden Sie eine Anzahl von Fragen. Es ist ein Fragebogen, der dazu dient, Ihre Zufriedenheit mit dem Klinikaufenthalt zu erfassen. Bitte füllen Sie Ihn aus. Sie brauchen dafür ungefähr 10 Minuten. ' +
-  'Kreuzen Sie bitte jeweils die Antwort an, die am ehesten auf Ihr Erleben beim Aufenthalt in der Psychiatrischen Klinik zutrifft. Die Umfrage ist anonym, Ihre Angaben lassen keinen Rückschluss auf Ihre Person zu. ' +
-  'Legen Sie bitte den ausgefüllten Fragebogen in das beiliegende Couvert und geben Sie das verschlossene Couvert vor dem Austritt beim Team-Büro ab. ' +
-  'Wir danken Ihnen für Ihre Mitarbeit! ' +
-  'Suchtfachklinik Zürich';
+  'Bitte teilen Sie uns Ihre Meinung zu Ihrem Aufenthalt in unserer Klinik mit. ' +
+  'Dieser kurze Fragebogen, der etwa 10 Minuten in Anspruch nimmt, zielt darauf ab, Ihre Erfahrungen und Zufriedenheit zu erfassen. ' +
+  'Kreuzen Sie bitte jeweils die Antwort an, die am ehesten auf Ihr Erleben beim Aufenthalt in der Psychiatrischen Klinik der SFK zutrifft. Ihre Antworten sind anonym und vertraulich. ' +
+  'Vielen Dank für Ihre Unterstützung! - Suchtfachklinik Zürich ';
 
 async function load() {
   try {
@@ -90,7 +109,46 @@ function evaluateSelectedValues() {
   display.value = false;
   showThankYouCard.value = true;
 }
+function previousPage() {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+}
+function nextPage() {
+  if (
+    currentPage.value * questionsPerPage <
+    selectedQuestionnaire.value.item.length
+  ) {
+    currentPage.value++;
+  }
+}
+const displayedQuestions = computed(() => {
+  const start = (currentPage.value - 1) * questionsPerPage;
+  const end = start + questionsPerPage;
+  return selectedQuestionnaire.value.item.slice(start, end);
+});
+
+const hasMorePages = computed(() => {
+  return (
+    currentPage.value * questionsPerPage <
+    selectedQuestionnaire.value.item.length
+  );
+});
+const isLastPage = computed(() => {
+  return (
+    currentPage.value * questionsPerPage >=
+    selectedQuestionnaire.value.item.length
+  );
+});
+const totalQuestions = computed(() => selectedQuestionnaire.value ? selectedQuestionnaire.value.item.length : 0);
+const answeredQuestions = computed(() =>
+  selectedQuestionnaire.value ? selectedQuestionnaire.value.item.filter(q => q.selected).length : 0
+);
+
+const progressValue = computed(() => answeredQuestions.value / totalQuestions.value);
+const progressLabel = computed(() => (progressValue.value * 100).toFixed(2) + '%');
 </script>
+
 
 <style scoped>
 /* Eigene Stile */
