adb03f5264798c6eae284f18ba0a955ea90994d5
karinke1234 <kennk1@bfh.ch>
2024-01-01 12:33:44 +0100

    update

--- src/pages/QuestionnairePage.vue
+++ src/pages/QuestionnairePage.vue
@@ -2,18 +2,46 @@
   <!-- q-card mit Bild und Text wird nur angezeigt, wenn display true ist -->
   <div v-if="loaded && display" class="q-mb-md">
     <q-card flat bordered>
-      <img
-        src="https://www.gutenachrichten.org/wp-content/uploads/schluessel-zur-zufriedenheit.jpg"
-      />
+      <img :src="questionnaireDetails.imageSrc" />
       <q-card-section>
-        <div class="text-h6">
-          Zürcher Patientenzufriedenheits-Fragebogen für Psychiatrie
-        </div>
-        <div class="text-subtitle2">(Züpaz)</div>
+        <div class="text-h6">{{ questionnaireDetails.title }}</div>
+        <div class="text-subtitle2">{{ questionnaireDetails.subtitle }}</div>
       </q-card-section>
-      <q-card-section class="q-pt-none">{{ content }}</q-card-section>
+      <q-card-section class="q-pt-none">{{ questionnaireDetails.content }}</q-card-section>
     </q-card>
   </div>
+
+  <!-- Likert-Skala nur für den BSCL-Fragebogen -->
+ <div v-if="loaded && display && props.title === 'BSCL V1'" class="q-pa-md">
+  <div class="flex justify-around text-center">
+    <div class="q-mx-xs">
+      <q-icon name="sentiment_very_dissatisfied" size="md" />
+      <div>0</div>
+      <div>überhaupt nicht</div>
+    </div>
+    <div class="q-mx-xs">
+      <q-icon name="sentiment_dissatisfied" size="md" />
+      <div>1</div>
+      <div>ein wenig</div>
+    </div>
+    <div class="q-mx-xs">
+      <q-icon name="sentiment_neutral" size="md" />
+      <div>2</div>
+      <div>ziemlich</div>
+    </div>
+    <div class="q-mx-xs">
+      <q-icon name="sentiment_satisfied" size="md" />
+      <div>3</div>
+      <div>stark</div>
+    </div>
+    <div class="q-mx-xs">
+      <q-icon name="sentiment_very_satisfied" size="md" />
+      <div>4</div>
+      <div>sehr stark</div>
+    </div>
+  </div>
+</div>
+
   <!-- Fortschrittsbalken -->
   <div class="q-pa-md" v-if="loaded && display">
     <q-linear-progress size="25px" :value="progressValue" color="primary">
@@ -89,11 +117,26 @@ const showThankYouCard = ref(false);
 const currentPage = ref(1);
 const questionsPerPage = 5;
 
-const content =
-  'Bitte teilen Sie uns Ihre Meinung zu Ihrem Aufenthalt in unserer Klinik mit. ' +
-  'Dieser kurze Fragebogen, der etwa 10 Minuten in Anspruch nimmt, zielt darauf ab, Ihre Erfahrungen und Zufriedenheit zu erfassen. ' +
-  'Kreuzen Sie bitte jeweils die Antwort an, die am ehesten auf Ihr Erleben beim Aufenthalt in der Psychiatrischen Klinik der SFK zutrifft. Ihre Antworten sind anonym und vertraulich. ' +
-  'Vielen Dank für Ihre Unterstützung! - Suchtfachklinik Zürich ';
+// Dynamische Inhalte und Bilder basierend auf dem Fragebogentitel
+const questionnaireDetails = computed(() => {
+  if (props.title === 'Zupaz V4') {
+    return {
+      imageSrc: 'https://www.flixcheck.de/wp-content/uploads/2022/03/kundenzufriedenheit-messen.jpeg',
+      content: 'Bitte teilen Sie uns Ihre Meinung zu Ihrem Aufenthalt in unserer Klinik mit. ' + 'Dieser kurze Fragebogen, der etwa 10 Minuten in Anspruch nimmt, zielt darauf ab, Ihre Erfahrungen und Zufriedenheit zu erfassen. ' + 'Kreuzen Sie bitte jeweils die Antwort an, die am ehesten auf Ihr Erleben beim Aufenthalt in der Psychiatrischen Klinik der SFK zutrifft. Ihre Antworten sind anonym und vertraulich. ' + 'Vielen Dank für Ihre Unterstützung! - Suchtfachklinik Zürich',
+      title: 'Zürcher Patientenzufriedenheits-Fragebogen für Psychiatrie',
+      subtitle: '(Züpaz)'
+    };
+  } else if (props.title === 'BSCL V1') {
+    return {
+      imageSrc: 'https://www.commma.de/wp-content/uploads/2021/08/Selbstreflexion-coaching-commma-fuehrung.jpg',
+      content: 'Hier finden Sie eine Liste verschiedener Probleme und Beschwerden, die gelegentlich auftreten können. Bitte lesen Sie jede Frage sorgfältig durch und bewerten Sie, wie sehr Sie in den letzten sieben Tagen bis heute durch diese Beschwerden beeinträchtigt wurden. Es ist wichtig, dass Sie nicht überlegen, welche Antworten den „besten Eindruck“ hinterlassen könnten, sondern ehrlich antworten, wie es für Sie persönlich zutrifft. Wählen Sie bitte bei jeder Frage die Antwort, die am besten auf Sie zutrifft.',
+      title: 'Fragebogen zur Erfassung von Problemen und Beschwerden',
+      subtitle: '(BSCL)'
+    };
+  }
+  return { imageSrc: '', content: '', title: '', subtitle: '' };
+});
+
 
 async function load() {
   try {
