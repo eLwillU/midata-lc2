d12764f0fcd833745527dbe74dad0e9618e17b41
Dominic Willi <dominic.willi@students.bfh.ch>
2024-01-09 15:20:18 +0100

    updated small

--- src/pages/WeeklyView.vue
+++ src/pages/WeeklyView.vue
@@ -1,27 +1,64 @@
 <template>
-  <div>Hello World</div>
+  <div>Wochenplan</div>
 
   <q-markup-table separator="cell">
     <thead>
-      <tr>
-        <th>Zeit</th>
-        <th>Montag</th>
-        <th>Dienstag</th>
-      </tr>
+      <th>Zeit</th>
+      <th v-for="d in weekDays" :key="d">
+        {{ d }}
+      </th>
     </thead>
     <tbody>
-      <tr>
-        <td style="height: 5em">0800-1200</td>
-        <td>Yeet</td>
-        <td>Yeet2</td>
-      </tr>
-      <tr>
-        <td style="height: 2em">0800-1200</td>
-        <td style="height: 5em">Yeet</td>
-        <td>Yeet2</td>
+      <tr v-for="e in events" :key="e">
+        <td>{{ e.time }}</td>
+        <td
+          colspan="5"
+          style="text-align: left"
+          class="bg-blue-2"
+          v-if="e.weekly"
+        >
+          <q-card style="height: 10em" flat class="bg-blue-2">
+            {{ e.description }}</q-card
+          >
+        </td>
+        <td v-if="!e.weekly" rowspan="2">
+          <q-card style="height: 10em" flat> {{ e.description }}</q-card>
+        </td>
       </tr>
     </tbody>
   </q-markup-table>
 </template>
 
-<script setup></script>
+<script setup>
+const weekDays = ['Montag'];
+const events = [
+  {
+    description: 'Gemeinsames Frühstück Haus 14 Entwöhnung',
+    time: '07:00 - 08:00',
+    weekly: true,
+  },
+  {
+    description: 'Gemeinsamer Morgen Spaziergang ALLE',
+    time: '08:15 - 08:45',
+    weekly: true,
+  },
+  {
+    description: 'Morgenrunde Haus 18 Entzug',
+    time: '08:45 - 09:15',
+    weekly: true,
+  },
+  {
+    description: 'Morgenrunde Haus 18 Entzug',
+    time: '08:45 - 09:15',
+    weekly: true,
+  },
+  {
+    description: 'Morgenrunde Haus 18 Entzug',
+    time: '08:45 - 09:15',
+    weekly: false,
+  },
+];
+
+console.log(events, weekDays);
+</script>
+<style></style>
